b2fd899a9702ce99b461895e1784f977da4ba86e
اله <youssef.123vrn.amr@gmail.com>
2026-07-10 16:01:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10720,7 +10720,7 @@ function slStopAllAnimations() {
               var mimeMatch = /^data:([^;]+);base64/.exec(dataUrl);
               var mimeType = mimeMatch ? mimeMatch[1] : (file.type || 'image/jpeg');
 
-              var visionRes = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent', {
+              var visionRes = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', 'x-goog-api-key': geminiKey },
                 body: JSON.stringify({
